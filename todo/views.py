a64692d3a354314450a33149ec000c4f24ddd275
Aliksandrmax <88476601+vangola91@users.noreply.github.com>
2022-04-27 12:16:52 +0500

    login

--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,12 @@
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
     return render(request, 'todo/home.html')
-
-
 
 def signupuser(request):
     if request.method == 'GET':
@@ -21,16 +19,26 @@ def signupuser(request):
                 user.seve()
                 login(request, user)
                 return redirect('currenttodos')
-
-
             except IntegrityError:
-                return render(request, 'todo/signupuser.html',
-                              {'form': UserCreationForm(), 'error': 'That username has already taken. Please choose a new username'})
+                return render(request, 'todo/signupuser.html',{'form': UserCreationForm(), 'error': 'That username has already taken. Please choose a new username'})
         else:
             return render(request, 'todo/signupuser.html', {'form':UserCreationForm(),'error': 'Passwords did not match'})
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), "error":"Usernema and password dod not match"})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
 
 
 def logoutuser(request):
